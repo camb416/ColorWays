001a973bab907b76e3e09651ace5e2ab64dba121
Cameron Browning <browning@wearesubrosa.com>
2016-06-28 11:29:49 -0400

    spin on its own

--- src/com/cameronbrowning/colorways/NoiseCube.java
+++ src/com/cameronbrowning/colorways/NoiseCube.java
@@ -52,8 +52,10 @@ public class NoiseCube extends PGraphics3D {
         background(0);
         pushMatrix();
         translate(width/2,height/2);
-        rotateX(p.mouseY/10.0f);
-        rotateY(p.mouseX/10.0f);
+        rotateY((float)myFrameCount/1000.0f);
+        rotateX((float)Math.cos(myFrameCount/100.0f)/50.0f+20.0f);
+//        rotateX(p.mouseY/10.0f);
+//        rotateY(p.mouseX/10.0f);
 
         for (int y = 0; y < 32; y++) {
             for (int x = 0; x < 32; x++) {
@@ -64,7 +66,7 @@ public class NoiseCube extends PGraphics3D {
                  //   point(x*10-160,y*10-160, z*10-160);
                 pushMatrix();
                     translate(x*10-160,y*10-160,z*10-160);
-                    box(green(color(palette.pixels[(int)(p.noise((float)x/10.0f,(float)y/10.0f,(float)z/10.0f)*255)+myFrameCount] ))/35.0f);
+                    box(green(color(palette.pixels[(int)(p.noise((float)x/10.0f,(float)y/10.0f,(float)z/10.0f)*255)+myFrameCount] ))/((float)Math.sin(myFrameCount/100.0f)*50.0f+76.0f));
                 popMatrix();
                 }
 
